17bb2c6237ffb6018a9e2cefb62d51063fcebdb7
Patrick Dornian <patrick.dornian@gmail.com>
2020-12-03 14:46:22 -0700

    tossed the pickle file and built the model training into the app because i don't feel like rewriting things into a module to address dependencies

--- app/app.py
+++ app/app.py
@@ -3,17 +3,22 @@ import requests
 import numpy as np 
 import pandas as pd
 import pickle
-import re
-import sklearn 
+
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 import re
 
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
+from sklearn.naive_bayes import BernoulliNB
+
 app = Flask(__name__, static_url_path='/static')
-#models and associated saved files
-#the trained pickle model doesn't seem to record the stemmer in the pipeline and otherwise breaks on import
-#so defining the stemmer manually before import
+
+#I'm just training the damn model here due to dependency issues in loading a pickled pipeline.
+
 snow=SnowballStemmer("english")
 def snowball_tokens2(text):
     text_processed = re.sub(r'[^A-Za-z]', ' ', text).split()
@@ -21,7 +26,29 @@ def snowball_tokens2(text):
     return tokens
 final_model = pickle.load(open('./models/final_model.p','rb'))
 
+canada_df = pd.read_csv('..\\data\\canada_subreddit_comments.csv')
 
+custom_stopwords= stopwords.words('english')
+custom_stopwords.extend(['people', 'like', 'canada'])
+custom_stopwords = [snow.stem(word) for word in custom_stopwords]
+extra_stopwords=['get', 'would', 'gt', 'one', 'go', 'make', 
+                 'actual', 'also', 'back', 'us', 'use', 'could', 'say', 'said', 'see', 'back', 'come',
+                'canadian', 'look']
+
+custom_stopwords.extend(extra_stopwords)
+
+X=canada_df['body_processed']
+y=canada_df['subreddit_bin']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=1920)
+
+bnb_final= Pipeline([('cv', CountVectorizer(tokenizer=snowball_tokens2,
+                                            max_features=4000,
+                                            stop_words=custom_stopwords,
+                                            binary=True)), 
+                     ('bnb', BernoulliNB())])
+
+
+bnb_final.fit(X_train, y_train);
 
 #=========================FUNCTIONS================================================
 def pred_post(text, model):
@@ -45,7 +72,7 @@ def index():
 @app.route('/classify', methods=["POST"])
 def classify():
 	comment = request.form.get('comment')
-	pred_text=pred_post(comment, final_model)
+	pred_text=pred_post(comment, bnb_final)
 	return render_template('index.html', classification=pred_text)
 
 # Call app.run(debug=True) when python script is called
